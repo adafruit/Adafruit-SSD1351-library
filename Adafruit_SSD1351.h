52c4d1494bce100ff07efa7ea61ac636e90091e8
ladyada <limor@ladyada.net>
2012-03-09 20:23:31 -0500

    first attempt at arduino library for SSD1351 oled driver - init & fillrect works!

---
+++ Adafruit_SSD1351.h
@@ -0,0 +1,143 @@
+/*************************************************** 
+  This is a library for the 0.96" 16-bit Color OLED with SSD1331 driver chip
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/684
+
+  These displays use SPI to communicate, 4 or 5 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#define swap(a, b) { uint16_t t = a; a = b; b = t; }
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+// Select one of these defines to set the pixel color order
+#define SSD1351_COLORORDER_RGB
+// #define SSD1351_COLORORDER_BGR
+
+#if defined SSD1351_COLORORDER_RGB && defined SSD1351_COLORORDER_BGR
+  #error "RGB and BGR can not both be defined for SSD1351_COLORODER."
+#endif
+
+// Timing Delays
+#define SSD1351_DELAYS_HWFILL		(3)
+#define SSD1351_DELAYS_HWLINE       (1)
+
+// SSD1351 Commands
+#define SSD1351_CMD_SETCOLUMN 		0x15
+#define SSD1351_CMD_SETROW    		0x75
+#define SSD1351_CMD_WRITERAM   		0x5C
+#define SSD1351_CMD_READRAM   		0x5D
+#define SSD1351_CMD_SETREMAP 		0xA0
+#define SSD1351_CMD_STARTLINE 		0xA1
+#define SSD1351_CMD_DISPLAYOFFSET 	0xA2
+#define SSD1351_CMD_DISPLAYALLOFF 	0xA4
+#define SSD1351_CMD_DISPLAYALLON  	0xA5
+#define SSD1351_CMD_NORMALDISPLAY 	0xA6
+#define SSD1351_CMD_INVERTDISPLAY 	0xA7
+#define SSD1351_CMD_FUNCTIONSELECT 	0xAB
+#define SSD1351_CMD_DISPLAYOFF 		0xAE
+#define SSD1351_CMD_DISPLAYON     	0xAF
+#define SSD1351_CMD_PRECHARGE 		0xB1
+#define SSD1351_CMD_DISPLAYENHANCE	0xB2
+#define SSD1351_CMD_CLOCKDIV 		0xB3
+#define SSD1351_CMD_SETVSL 		0xB4
+#define SSD1351_CMD_SETGPIO 		0xB5
+#define SSD1351_CMD_PRECHARGE2 		0xB6
+#define SSD1351_CMD_SETGRAY 		0xB8
+#define SSD1351_CMD_USELUT 		0xB9
+#define SSD1351_CMD_PRECHARGELEVEL 	0xBB
+#define SSD1351_CMD_VCOMH 		0xBE
+#define SSD1351_CMD_CONTRASTABC		0xC1
+#define SSD1351_CMD_CONTRASTMASTER	0xC7
+#define SSD1351_CMD_MUXRATIO            0xCA
+#define SSD1351_CMD_COMMANDLOCK         0xFD
+#define SSD1351_CMD_HORIZSCROLL         0x96
+#define SSD1351_CMD_STOPSCROLL          0x9E
+#define SSD1351_CMD_STARTSCROLL         0x9F
+
+class Adafruit_SSD1351 : public Print {
+ public:
+  Adafruit_SSD1351(uint8_t CS, uint8_t RS, uint8_t SID, uint8_t SCLK, uint8_t RST);
+  Adafruit_SSD1351(uint8_t CS, uint8_t RS, uint8_t RST);
+
+  uint16_t Color565(uint8_t r, uint8_t g, uint8_t b);
+
+  // drawing primitives!
+  void drawPixel(uint16_t x, uint16_t y, uint16_t color);
+  void fillScreen(uint16_t color);
+  void drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color);
+  void drawFastLine(uint16_t x0, uint16_t y0, uint16_t l, uint16_t color, uint8_t flag);
+  void drawVerticalLine(uint16_t x0, uint16_t y0, uint16_t length, uint16_t color);
+  void drawHorizontalLine(uint16_t x0, uint16_t y0, uint16_t length, uint16_t color);
+  void drawTriangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+  void fillTriangle(int32_t x0, int32_t y0, int32_t x1, int32_t y1, int32_t x2, int32_t y2, uint16_t color);
+  void drawRect(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h, uint16_t color);
+  void fillRect(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h, uint16_t color);
+  void drawRoundRect(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h, uint16_t radius, uint16_t color);
+  void fillRoundRect(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h, uint16_t radius, uint16_t color);
+  void drawCircle(uint16_t x0, uint16_t y0, uint16_t r,	uint16_t color);
+  void fillCircle(uint16_t x0, uint16_t y0, uint16_t r,	uint16_t color);
+
+  void setCursor(uint16_t x, uint16_t y);
+  void setTextColor(uint16_t c);
+  void setTextSize(uint8_t s);
+
+  void invert(boolean);
+#if ARDUINO >= 100
+  virtual size_t write(uint8_t);
+#else
+  virtual void write(uint8_t);
+#endif
+
+  void drawChar(uint16_t x, uint16_t y, char c, uint16_t color, uint8_t s = 1);
+  void drawString(uint16_t x, uint16_t y, char *c, uint16_t color, uint8_t s = 1);
+
+  // commands
+  void initDisplay(void);
+  void goHome(void);
+  void goTo(int x, int y);
+
+  void reset(void);
+
+  /* low level */
+
+  void writeData(uint8_t d);
+  void writeCommand(uint8_t c);
+
+  uint16_t width();
+  uint16_t height();
+
+  static const uint16_t TFTWIDTH = 128;
+  static const uint16_t TFTHEIGHT = 128;
+
+  void writeData_unsafe(uint16_t d);
+
+  void setWriteDir(void);
+  void write8(uint8_t d);
+
+ private:
+  void drawCircleHelper(uint16_t x0, uint16_t y0, uint16_t r, uint8_t corner, uint16_t color);
+  void fillCircleHelper(uint16_t x0, uint16_t y0, uint16_t r, uint8_t corner, uint16_t delta, uint16_t color);
+  void spiwrite(uint8_t);
+
+  uint8_t _cs, _rs, _rst, _sid, _sclk;
+  uint8_t csport, rsport, sidport, sclkport;
+  uint8_t cspin, rspin, sidpin, sclkpin;
+
+  uint16_t _width, _height;
+  uint8_t textsize;
+  uint16_t cursor_x, cursor_y;
+  uint16_t textcolor;
+};
---
+++ README.txt
@@ -0,0 +1,19 @@
+This is a library for the 0.96" 16-bit Color OLED with SSD1331 driver chip
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/684
+
+These displays use SPI to communicate, 4 or 5 pins are required to  
+interface
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above must be included in any redistribution
+
+To download. click the DOWNLOADS button in the top right corner, rename the uncompressed folder Adafruit_SSD1131. Check that the Adafruit_SSD1331 folder contains Adafruit_SSD1331.cpp and Adafruit_SSD1331.h
+
+Place the Adafruit_SSD1331 library folder your <arduinosketchfolder>/libraries/ folder. You may need to create the libraries subfolder if its your first library. Restart the IDE.
