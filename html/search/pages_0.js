9f11b09f942e059d8ada9e657ce7d36ed02a1a36
Travis CI <travis@travis-ci.org>
2019-04-07 07:31:14 +0000

    Deploy code docs to GitHub Pages Travis build: 8
    Commit: 4f2477c8c2ed3c07ecc3115a28276eec29b53d6a

--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['arduino_20library_20for_20color_20oleds_20based_20on_20ssd1351_20drivers_2e',['Arduino library for color OLEDs based on SSD1351 drivers.',['../index.html',1,'']]],
-  ['adafruit_20ssd1351_20arduino_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_2dssd1351_2dlibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_2dssd1351_2dlibrary_29',['Adafruit SSD1351 Arduino Library [![Build Status](https://travis-ci.org/adafruit/Adafruit-SSD1351-library.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit-SSD1351-library)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['arduino_20library_20for_20color_20oleds_20based_20on_20ssd1351_20drivers_2e',['Arduino library for color OLEDs based on SSD1351 drivers.',['../index.html',1,'']]]
 ];
